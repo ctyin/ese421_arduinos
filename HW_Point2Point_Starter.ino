1a39e0287a20555916173b2542da2537a051b9d2
Christopher Yin <ctyin@seas.upenn.edu>
2021-10-05 14:11:39 -0400

    increased velocity and added derivative

--- HW_Point2Point_Starter.ino
+++ HW_Point2Point_Starter.ino
@@ -1,4 +1,5 @@
 #include <Romi32U4.h>
+#include <math.h>
 
 Romi32U4LCD lcd;
 Romi32U4Buzzer buzzer;
@@ -50,11 +51,18 @@ struct robotPose{
 //
 // this is dummy code -- will be replaced by actual pose measurement
 //
-struct robotPose getPose() {
-  struct robotPose measuredP;
-  measuredP.X = 0.8516*millis()/25.0;
-  measuredP.Y = measuredP.X/1.732;
-  measuredP.Q = 30.0;
+struct robotPose getPose(float deltaT, int L_robot) {
+  static struct robotPose measuredP;
+  float countLeft = encoders.getCountsAndResetLeft();
+  float countRight = encoders.getCountsAndResetRight();
+  float vLeft = PI * 6.9 * (countLeft/1440.0)/deltaT;
+  float vRight = PI * 6.9 * (countRight/1440.0)/deltaT;
+  float vAvg = (vLeft + vRight ) / 2;
+  
+  measuredP.X += deltaT * vAvg * cos(measuredP.Q);
+  measuredP.Y += deltaT * vAvg * sin(measuredP.Q);
+  measuredP.Q += deltaT * (vRight - vLeft) / L_robot;
+
   return measuredP;
 }
 void setup() {
@@ -64,17 +72,21 @@ void setup() {
 }
 
 void loop() {
-  const float x_error = 0.1;
-  const float y_error = 0.1;
-  struct robotPose P = getPose(); // pretend this gives actual pose
+  static float last_thetad = 0.0;
+  const float x_error = 10;
+  const float y_error = 10;
+  const float slowdown_threshold = 15;
+  float deltaT = 0.05;
+  const int L_robot = 14;
+  struct robotPose P = getPose(deltaT, L_robot); // pretend this gives actual pose
   static int index = 0;
-  static float x_way_pts[7] = {5, 10, 15, 20, 25}; // first way point is the starting position
-  static float y_way_pts[7] = {30, 20, 15, 50, 100};
+  const int ARR_LEN = 7;
+  static float x_way_pts[ARR_LEN] = {0, 63, 63, 0, 0}; // first way point is the starting position
+  static float y_way_pts[ARR_LEN] = {0, 0, 63, 63, 0};
   static float last_x = 0;
   static float last_y = 0;
 
   static unsigned long millisLast = millis();
-  float deltaT = 0.05;
   while ((millis() - millisLast) < 1000 * deltaT) {}
   millisLast = millis();
 
@@ -94,17 +106,34 @@ void loop() {
     last_x = x_way_pts[index];
     last_y = y_way_pts[index];
     index++;
+
+    if (index > ARR_LEN) {
+      exit(0);
+    }
   }
 
-  // Doing some math to calculate the diff between the shortest path line
-  float slope = (y_way_pts[index] - last_y) / (x_way_pts[index] - last_x);
-  float intercept = last_y - slope * last_x;
+  int pwmAvg = 100;
 
-  // Using a manipulated point-to-line distance formula
-  float dist_err = (-1 * slope * P.X + P.Y - intercept) / sqrt(pow(slope, 2) + 1);
-  int pwmDel = (int) (2.2 * dist_err);
+  float theta_d = atan2(y_way_pts[index] - P.Y, x_way_pts[index] - P.X);
+  float f = theta_d - P.Q;
+  if (f > PI) {
+    f = f - TWO_PI;
+  } else if (f < -1 * PI) {
+    f = f + TWO_PI;
+  }
+
+  int pwmDel = (int) (50.0 * f - 10.0 * (f - last_thetad)/deltaT;
+  last_thetad = f;
   pwmDel = constrain(pwmDel, -100, 100);
+  
+  float dist_to_next = sqrt(pow(P.X - x_way_pts[index], 2) + pow(P.Y - y_way_pts[index], 2));
+  if (dist_to_next < 10) {
+    pwmAvg += (int) (-3.5 * (20 - dist_to_next));
+  }
 
-  int pwmAvg = 50;
-  if (
+  if (motorsGo) {
+    motors.setSpeeds(pwmAvg - pwmDel, pwmAvg + pwmDel);
+  } else {
+    motors.setSpeeds(0, 0);
+  }
 }
